c15d20672ec55fa7dd34e6b1d4e526cc4cb52d5e
nguyenmanhdang90 <nguyenmanhdang90@gmail.com>
2019-01-09 18:52:56 +0700

    refactor

--- src/bai04M2/Vd/Vd1.java
+++ src/bai04M2/Vd/Vd1.java
@@ -1,4 +1,4 @@
-package bai04M2.thucHanh;
+package bai04M2.Vd;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,18 +9,23 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-public class DocKhoa {
+public class Vd1 {
 
 	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db = dbf.newDocumentBuilder();
-		Document doc = db.parse(new File("t3h\\src\\bai04M2\\thucHanh\\khoa.xml"));
+		Document doc = db.parse(new File("t3h\\src\\bai04M2\\Vd\\khoa.xml"));
 		Element nutGoc = doc.getDocumentElement();
 		System.out.println(nutGoc.getNodeName());
 		System.out.println(nutGoc.getNodeType());
-		System.out.println(nutGoc.getChildNodes());
+		System.out.println(nutGoc.getChildNodes().getLength());
 		System.out.println(nutGoc.getElementsByTagName("makh").item(0).getTextContent());
+		NodeList nodeL = nutGoc.getChildNodes();
+		for(int i = 0; i <nodeL.getLength(); i++){
+			System.out.println(nodeL.item(i).getNodeName());
+		}
 	}
 }
